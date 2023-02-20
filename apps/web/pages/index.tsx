b4a771b9b493c216baab4efd459a07c1ece4e676
JSurapong <surapong.kawkangploo@gmail.com>
2023-02-20 15:36:22 +0700

    fetchBTC

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+
 import { Button, Header, Hero, CoinSection } from "ui";
 
 export default function Web() {
+  const [btc, setBtc] = useState(0);
+
+  async function getBitcoinPrice() {
+    const response = await fetch(
+      "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+    );
+    const data = await response.json();
+    const price = data.bpi.USD.rate_float;
+    const time = new Date(data.time.updated).toLocaleTimeString();
+
+    setBtc(price);
+
+    console.log(`Time : ${time}`, `The current price of Bitcoin is: $${price}`);
+  }
+  useEffect(() => {
+    getBitcoinPrice();
+    const interval = setInterval(() => {
+      getBitcoinPrice();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Header
@@ -25,7 +50,7 @@ export default function Web() {
           },
         ]}
       />
-      <Hero image="./images/btc.png" title={"BTC price"} />
+      <Hero image="./images/btc.png" title={`BTC : ${btc}`} />
       <CoinSection title="ETH" image="./images/eth.png" id="eth" />
       <CoinSection title="DOGE" image="./images/doge.jpeg" id="doge" />
     </>
