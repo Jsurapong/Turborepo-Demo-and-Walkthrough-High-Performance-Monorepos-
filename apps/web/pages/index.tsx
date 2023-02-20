a6552ba212541538e15ac41a2823351cc629025e
JSurapong <surapong.kawkangploo@gmail.com>
2023-02-20 15:15:59 +0700

    update Header

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button, Header, Hero } from "ui";
 export default function Web() {
   return (
     <>
-      <Header menus={[{ title: "BTC" }, { title: "ETH" }]} />
+      <Header menus={[{ title: "BTC" }, { title: "ETH" }, { title: "DOGE" }]} />
       <Hero image="./images/btc.png" title={"BTC price"} />
     </>
   );
