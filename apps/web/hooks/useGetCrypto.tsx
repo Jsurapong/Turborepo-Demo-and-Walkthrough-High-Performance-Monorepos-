ec9598c010489e9b2d4b4e0ff96df253579fa41c
JSurapong <surapong.kawkangploo@gmail.com>
2023-02-20 16:08:54 +0700

    move to custom Hooks

---
+++ apps/web/hooks/useGetCrypto.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type UseGetCryptoProps = { sym: "BTC" | "ETH" | "DOGE" };
+
+export const useGetCrypto = ({ sym }: UseGetCryptoProps) => {
+  const [usd, setUsd] = useState(null);
+
+  const fetch = async () => {
+    const response = await axios(
+      `https://min-api.cryptocompare.com/data/price?fsym=${sym}&tsyms=USD`
+    );
+
+    const price = response.data.USD ?? null;
+
+    setUsd(price);
+  };
+
+  useEffect(() => {
+    fetch();
+
+    const interval = setInterval(() => {
+      fetch();
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return [usd];
+};
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,65 +1,13 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-import { Button, Header, Hero, CoinSection } from "ui";
+import { useGetCrypto } from "../hooks/useGetCrypto";
+
+import { Header, Hero, CoinSection } from "ui";
 
 export default function Web() {
-  const [btc, setBtc] = useState(0);
-  const [eth, setEth] = useState(0);
-  const [doge, setDoge] = useState(0);
-
-  async function getBitcoinPrice() {
-    const response = await axios(
-      "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"
-    );
-
-    const price = response.data.USD;
-
-    setBtc(price);
-  }
-
-  async function getEthereumPrice() {
-    const response = await axios(
-      "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
-    );
-
-    const price = response.data.USD;
-
-    setEth(price);
-  }
-
-  async function getDogeCoinPrice() {
-    const response = await axios(
-      "https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=USD"
-    );
-
-    const price = response.data.USD;
-
-    setDoge(price);
-  }
-
-  useEffect(() => {
-    getBitcoinPrice();
-    const interval = setInterval(() => {
-      getBitcoinPrice();
-    }, 10000);
-
-    getEthereumPrice();
-    const interval2 = setInterval(() => {
-      getEthereumPrice();
-    }, 10000);
-
-    getDogeCoinPrice();
-    const interval3 = setInterval(() => {
-      getDogeCoinPrice();
-    }, 10000);
-
-    return () => {
-      clearInterval(interval);
-      clearInterval(interval2);
-      clearInterval(interval3);
-    };
-  }, []);
+  const [btc] = useGetCrypto({ sym: "BTC" });
+  const [eth] = useGetCrypto({ sym: "ETH" });
+  const [doge] = useGetCrypto({ sym: "DOGE" });
 
   return (
     <>
