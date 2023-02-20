f81a9983b3c35cb1b313265068f35f7ecf93b4b0
JSurapong <surapong.kawkangploo@gmail.com>
2023-02-20 15:24:30 +0700

    add eth,doge section

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Button, Header, Hero } from "ui";
+import { Button, Header, Hero, CoinSection } from "ui";
 
 export default function Web() {
   return (
     <>
       <Header menus={[{ title: "BTC" }, { title: "ETH" }, { title: "DOGE" }]} />
       <Hero image="./images/btc.png" title={"BTC price"} />
+      <CoinSection title="ETH" image="./images/eth.png" />
+      <CoinSection title="DOGE" image="./images/doge.jpeg" />
     </>
   );
 }
---
+++ packages/ui/CoinSection.tsx
@@ -0,0 +1,24 @@
+type CoinSectionProps = {
+  image?: string;
+  title: string;
+};
+
+export const CoinSection = ({ image, title }: CoinSectionProps) => {
+  return (
+    <section id="productive" className="bg-white dark:bg-darkBlue">
+      {/* <!-- Productive Container --> */}
+      <div className="container flex flex-col items-center mx-auto px-6 pt-24 pb-32 md:flex-row md:space-x-16">
+        {/* <!-- Image --> */}
+        <div className="md:w-1/2">
+          {image && <img src={image} alt="" className="mb-10" />}
+        </div>
+        {/* <!-- Content --> */}
+        <div className="flex flex-col items-start md:w-1/2">
+          <div className="flex flex-col space-y-5">
+            <h4 className="max-w-md text-xl font-bold md:text-4xl">{title}</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
