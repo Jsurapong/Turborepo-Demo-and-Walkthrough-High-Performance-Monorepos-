af9c1b52e8f578647786e5e552c0803cef47ba64
JSurapong <surapong.kawkangploo@gmail.com>
2023-02-21 12:33:13 +0700

    add dymanic framework

--- apps/web/coin-config.js
+++ apps/web/coin-config.js
@@ -2,45 +2,88 @@
  * This file is expected to be used in next.config.js only
  */
 
-const path = require('path')
-const fs = require('fs')
-const merge = require('deepmerge')
-const prettier = require('prettier')
-const core = require('coin/config.cjs')
+const path = require("path");
+const fs = require("fs");
+const merge = require("deepmerge");
+const prettier = require("prettier");
+const core = require("coin/config.cjs");
 
-const PROVIDERS = [
-    'local',
-    'cryptocompare'
-]
+const PROVIDERS = ["local", "cryptocompare"];
 
 function getProviderName() {
-    return (
-        process.env.COINE_PROVIDER || 'local'
-    )
+    return process.env.COINE_PROVIDER || "local";
 }
 
 function withCoinConfig(nextConfig = {}) {
-    const config = merge(
-        { coin: { provider: getProviderName() } },
-        nextConfig
-    )
-    const { coin } = config
-    const { provider } = coin
+    const config = merge({ coin: { provider: getProviderName() } }, nextConfig);
+    const { coin } = config;
+    const { provider } = coin;
 
     if (!provider) {
         throw new Error(
             `The coin provider is missing, please add a valid provider name or its environment variables`
-        )
+        );
     }
     if (!PROVIDERS.includes(provider)) {
         throw new Error(
             `The coin provider "${provider}" can't be found, please use one of "${PROVIDERS.join(
-                ', '
+                ", "
             )}"`
-        )
+        );
     }
 
-    return core.withCoineConfig(config)
+    // Update paths in `tsconfig.json` to point to the selected provider
+    if (coin.updateTSConfig !== false) {
+        const tsconfigPath = path.join(
+            process.cwd(),
+            coin.tsconfigPath || "tsconfig.json"
+        );
+        const tsconfig = require(tsconfigPath);
+
+        console.log({ tsconfigPath })
+
+        // The module path is a symlink in node_modules
+        // -> /node_modules/[name]/dist/index.js
+        const absolutePath = require.resolve(provider);
+        // but we want references to go to the real path in /packages instead
+        // -> packages/[name]/dist
+        const distPath = path.join(
+            path.relative(process.cwd(), absolutePath),
+            ".."
+        );
+
+        // -> /packages/[name]/src
+        const modulePath = path.join(distPath, "../src");
+
+        tsconfig.compilerOptions.paths["@framework"] = [`${modulePath}`];
+        tsconfig.compilerOptions.paths["@framework/*"] = [`${modulePath}/*`];
+
+        fs.writeFileSync(
+            tsconfigPath,
+            prettier.format(JSON.stringify(tsconfig), { parser: "json" })
+        );
+
+        const webpack = config.webpack;
+
+        // // To improve the DX of using references, we'll switch from `src` to `dist`
+        // // only for webpack so imports resolve correctly but typechecking goes to `src`
+        config.webpack = (cfg, options) => {
+            if (Array.isArray(cfg.resolve.plugins)) {
+                const jsconfigPaths = cfg.resolve.plugins.find(
+                    (plugin) => plugin.constructor.name === "JsConfigPathsPlugin"
+                );
+
+                if (jsconfigPaths) {
+                    jsconfigPaths.paths["@framework"] = [distPath];
+                    jsconfigPaths.paths["@framework/*"] = [`${distPath}/*`];
+                }
+            }
+
+            return webpack ? webpack(cfg, options) : cfg;
+        };
+    }
+
+    return core.withCoineConfig(config);
 }
 
-module.exports = { withCoinConfig, getProviderName }
+module.exports = { withCoinConfig, getProviderName };
